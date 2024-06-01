b46266a4fd3bc8dad2038478b24a05f04fdf1335
gorik <delavarus@gmail.com>
2024-06-01 13:25:15 +0300

    screen_home

--- app/src/main/java/by/gorik/newsapp/navigation/Screen.kt
+++ app/src/main/java/by/gorik/newsapp/navigation/Screen.kt
@@ -1,26 +1,38 @@
 package by.gorik.newsapp.navigation
 
-sealed class Screen(val route :String) {
+sealed class Screen(val route: String,val name:String = "") {
 
-object NewsOnline:Screen(route = "newsonline")
-object NewsOffline:Screen(route = "newsoffline")
-object NewsSearch:Screen(route = "newssearch")
-object NewsBySource:Screen(route = "newsbysource?sourceId={sourceId}&countryCode={countryCode}&languageId={languageId}"){
-    fun passData(
-        sourceId:String= "",
-        countryCode:String= "",
-        languageId:String= "",
-    ):String{
-        return "newsbysource?sourceId=$sourceId&countryCode=$countryCode&languageId=$languageId"
+    object NewsOnline : Screen(route = "newsonline",name = "NEWS ONLINE")
+    object NewsOffline : Screen(route = "newsoffline",name = "NEWS OFFLINE")
+    object NewsSearch : Screen(route = "newssearch",name = "NEWS SEARCH")
+    object NewsBySource :
+        Screen(route = "newsbysource?sourceId={sourceId}&countryCode={countryCode}&languageId={languageId}",name = "NEWS BY SOURCES") {
+        fun passData(
+            sourceId: String = "",
+            countryCode: String = "",
+            languageId: String = "",
+        ): String {
+            return "newsbysource?sourceId=$sourceId&countryCode=$countryCode&languageId=$languageId"
+        }
     }
-}
-object Sources:Screen(route = "sources")
-object Language:Screen(route = "language")
-    object Country:Screen(route = "country")
-    object HomeScreen:Screen(route = "homescreen")
-    object NewsPagination:Screen(route = "newspagination")
+
+    object Sources : Screen(route = "sources",name = "SOURCES")
+    object Language : Screen(route = "language",name = "LANGUAGES")
+    object Country : Screen(route = "country",name = "COUNTRYS")
+    object HomeScreen : Screen(route = "homescreen",name = "HOMESCREEN")
+    object NewsPagination : Screen(route = "newspagination",name = "NEWS PAGINATION")
 
 
+    companion object{
 
-
+        val all_screen = listOf(
+            NewsOnline,
+            NewsOffline,
+            NewsSearch,
+            Sources,
+            Language,
+            Country,
+            NewsPagination
+        )
+    }
 }
--- app/src/main/java/by/gorik/newsapp/ui/HomeScreen.kt
+++ app/src/main/java/by/gorik/newsapp/ui/HomeScreen.kt
@@ -1,9 +1,78 @@
 package by.gorik.newsapp.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import by.gorik.newsapp.navigation.Screen
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        LazyColumn {
+            items(Screen.all_screen.size) { index ->
+
+                val screen = Screen.all_screen[index]
+
+                CustomButton(
+                    text = screen.name,
+                    onClick = { navController.navigate(screen.route) })
+
+
+            }
+        }
+
+    }
+
+
+}
+
+@Composable
+fun CustomButton(
+    text: String,
+    onClick: () -> Unit,
+    fontSize: TextUnit = 20.sp,
+) {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        color = MaterialTheme.colorScheme.primaryContainer,
+        shape = RoundedCornerShape(10.dp)
+    ) {
+
+        Button(
+            onClick = { onClick() },
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .padding(horizontal = 30.dp)
+
+        ) {
+            Text(text = text, fontSize = fontSize)
+
+        }
+
+
+    }
+
 
 }
