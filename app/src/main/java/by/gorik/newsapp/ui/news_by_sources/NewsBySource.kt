3516a3d253a145c14e8bdf2afdc0a5096359f704
gorik <delavarus@gmail.com>
2024-06-02 08:15:17 +0300

    screen_by_done

--- app/src/main/java/by/gorik/newsapp/ui/news_by_sources/NewsBySource.kt
+++ app/src/main/java/by/gorik/newsapp/ui/news_by_sources/NewsBySource.kt
@@ -1,9 +1,73 @@
 package by.gorik.newsapp.ui.news_by_sources
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import by.gorik.newsapp.data.model.ApiArticle
+import by.gorik.newsapp.ui.base.ArticleList
+import by.gorik.newsapp.ui.base.ShowError
+import by.gorik.newsapp.ui.base.ShowLoading
+import by.gorik.newsapp.ui.base.UiState
 
 @Composable
-fun NewsBySourceRoute(navController: NavHostController, onNewsClick: (String) -> Unit) {
+fun NewsBySourceRoute(
+    navController: NavHostController,
+    vm: NewsByVm,
+    onNewsClick: (String) -> Unit,
+    sourceId: String? = null,
+    languageId: String? = null,
+    countryCode: String? = null,
+) {
+
+
+    LaunchedEffect(key1 = Unit, block = {
+        when {
+            !sourceId.isNullOrEmpty() -> {
+                vm.fetchBySource(sourceId)
+
+            }
+
+            !languageId.isNullOrEmpty() -> {
+                vm.fetchBySource(languageId)
+            }
+
+            !countryCode.isNullOrEmpty() -> {
+                vm.fetchBySource(countryCode)
+            }
+        }
+
+
+    })
+
+
+    val state = vm.uiState.collectAsStateWithLifecycle().value
+
+    Column {
+        NewsByScreen(onNewsClick, state)
+    }
+
+
+}
+
+@Composable
+fun NewsByScreen(onNewsClick: (String) -> Unit, state: UiState<List<ApiArticle>>) {
+
+
+    when (state) {
+        is UiState.Error -> {
+            val errMsg = state.mesg
+                ShowError(mesg =errMsg)
+        }
+
+        UiState.Loading -> {
+            ShowLoading()
+        }
+        is UiState.Success -> {
+            ArticleList(data = state.data, onNewsClick =onNewsClick )
+        }
+    }
 
 }
---
+++ app/src/main/java/by/gorik/newsapp/ui/news_by_sources/NewsByVm.kt
@@ -0,0 +1,120 @@
+package by.gorik.newsapp.ui.news_by_sources
+
+import androidx.lifecycle.viewModelScope
+import by.gorik.newsapp.data.model.ApiArticle
+import by.gorik.newsapp.data.model.Language
+import by.gorik.newsapp.data.repository.CountryRepo
+import by.gorik.newsapp.data.repository.NewArticlesRepo
+import by.gorik.newsapp.data.repository.NewsArticlesBYRepo
+import by.gorik.newsapp.ui.base.BaseVm
+import by.gorik.newsapp.ui.base.UiState
+import by.gorik.newsapp.utils.DispatcherProvider
+import by.gorik.newsapp.utils.ResourceProvider
+import by.gorik.newsapp.utils.logger.Logger
+import by.gorik.newsapp.utils.network.NetworkHelper
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectIndexed
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+
+class NewsByVm(
+    val newsRepo: NewsArticlesBYRepo,
+    val logger: Logger,
+    val networkHelper: NetworkHelper,
+    val dispatcherProvider: DispatcherProvider,
+    val resourceProvider: ResourceProvider
+) : BaseVm() {
+
+    private val _uiState = MutableStateFlow<UiState<List<ApiArticle>>>(UiState.Loading)
+    val uiState: StateFlow<UiState<List<ApiArticle>>> = _uiState
+
+
+    fun fetchBySource(source: String) {
+        viewModelScope.launch {
+
+            if (!networkHelper.isNetworkActive()) {
+                val noInternetMsg = resourceProvider.getStringInternetNotAvailable()
+                _uiState.value = UiState.Error(noInternetMsg)
+                logger.d(TAG, "No interne")
+                return@launch
+
+            } else {
+
+                val sourceCorrect = source?.replace("{", "")
+                    ?.replace("}", "")
+                logger.d(TAG, "source  query ::: $sourceCorrect")
+                sourceCorrect?.let { query ->
+                    _uiState.value = UiState.Loading
+                    newsRepo.getArticlesbySources(query)
+                        .catch { e ->
+                            _uiState.value = UiState.Error(e.toString())
+                            logger.d(TAG, "error while fetching news ::: " + e.message.toString())
+
+                        }.collect {
+                            _uiState.value = UiState.Success(it)
+                            logger.d(TAG, "by source articles ::: $it")
+                        }
+                }
+
+            }
+        }
+
+    }
+
+    fun fetchByCountry(country: String) {
+        viewModelScope.launch {
+
+            if (!networkHelper.isNetworkActive()) {
+                val noInternetMsg = resourceProvider.getStringInternetNotAvailable()
+                _uiState.value = UiState.Error(noInternetMsg)
+                logger.d(TAG, "No internet")
+                return@launch
+
+            } else {
+                _uiState.value = UiState.Loading
+                newsRepo.getArticlesbyCountry(country)
+                    .catch { e ->
+                        _uiState.value = UiState.Error(e.toString())
+                        logger.d(TAG, "error while fetching news ::: " + e.message.toString())
+
+                    }.collect {
+                        _uiState.value = UiState.Success(it)
+                        logger.d(TAG, "by country articles ::: $it")
+                    }
+            }
+
+        }
+    }
+
+    fun fetchByLanguage(language: String) {
+        viewModelScope.launch {
+
+            if (!networkHelper.isNetworkActive()) {
+                val noInternetMsg = resourceProvider.getStringInternetNotAvailable()
+                _uiState.value = UiState.Error(noInternetMsg)
+                logger.d(TAG, "No internet")
+                return@launch
+
+            } else {
+                _uiState.value = UiState.Loading
+                newsRepo.getArticlesbyLanguage(language)
+                    .catch { e ->
+                        _uiState.value = UiState.Error(e.toString())
+                        logger.d(TAG, "error while fetching news ::: " + e.message.toString())
+
+                    }.collect {
+                        _uiState.value = UiState.Success(it)
+                        logger.d(TAG, "by language articles ::: $it")
+                    }
+            }
+
+        }
+    }
+
+
+    companion object {
+        const val TAG = "NewsOnlineListVm"
+    }
+}
